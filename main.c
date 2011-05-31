b73d49b73a1ffc0144b4b7b6b0eccf4f2ae37884
Ludovic Lacoste <ludolacost@gmail.com>
2011-06-01 01:11:45 +0200

    Added some comments and a menu.

--- main.c
+++ main.c
@@ -9,9 +9,7 @@
 typedef struct {
     GtkWidget *fenetre;
         GtkWidget *vbox;
-            GtkWidget *hbox_menu;
-                GtkWidget *bt_nouvelle_partie;
-                GtkWidget *bt_quitter;
+            GtkWidget *menu;
             GtkWidget *aframe;
                 GtkWidget *table;
                     GtkWidget *bt_cellules;
@@ -24,16 +22,16 @@ typedef struct {
     JE_jeu jeu;
 } JE_contexte;
 
-void maj_etat_bouton(GtkWidget *bt, case_t c) {
+void maj_appar_bouton(GtkWidget *bt, case_t c) {
     GdkColor clr = {0};
 
-    if(c&CASE_GRISE) {
+    if(c&CASE_GRISE) {  // gris 50%
         clr.red = 32768;
         clr.green = 32768;
         clr.blue = 32768;
     }
 
-    if(c&CASE_PEUTCLIQUER) {
+    if(c&CASE_PEUTCLIQUER) {    // vert
         clr.red = 0;
         clr.green = 65535;
         clr.blue = 0;
@@ -74,7 +72,7 @@ void maj_etat(JE_contexte *ctx) {
     gtk_label_set_text(GTK_LABEL(ctx->lbl_statut), buffer);
 
     // mise à jour du plateau
-    maj_etat_bouton(ctx->bt_cellules, ctx->jeu.part[0]);
+    maj_appar_bouton(ctx->bt_cellules, ctx->jeu.part[0]);
 
     for(j=0; j<9; j++)
         for(i=0; i<9; i++) {
@@ -101,16 +99,28 @@ void maj_etat(JE_contexte *ctx) {
 
             gtk_button_set_label(GTK_BUTTON(bt), txt);
 
-            maj_etat_bouton(bt, c);
+            maj_appar_bouton(bt, c);
         }
 
-    maj_etat_bouton(ctx->bt_sortie, ctx->jeu.part[1]);
+    maj_appar_bouton(ctx->bt_sortie, ctx->jeu.part[1]);
 }
 
-void nouvelle_partie(GtkWidget *widget, JE_contexte *ctx) {
+void nouvelle_partie(GtkWidget *w, JE_contexte *ctx) {
     JE_nouvellepartie(&ctx->jeu);
 
     maj_etat(ctx);
+}
+
+void sauver_partie(GtkWidget *w, JE_contexte *ctx) {
+    // TODO
+}
+
+void charger_partie(GtkWidget *w, JE_contexte *ctx) {
+    // TODO
+}
+
+void quitter_partie(GtkWidget *w, JE_contexte *ctx) {
+    gtk_main_quit();
 }
 
 void action_pion(GtkWidget *widget, JE_contexte *ctx) {
@@ -131,15 +141,35 @@ void action_pion(GtkWidget *widget, JE_contexte *ctx) {
 
 printf("bouton%d,%d appuyé\n", x, y);
 
+    // on fait l'action demandée en fonction de si on vient de choisir le pion ou sa destination
     if(JE_etat(&ctx->jeu)&ETAT_ATTENTEBOUGER)
         JE_bougerpion(&ctx->jeu, x, y);
     else
         JE_selectpion(&ctx->jeu, x, y);
 
+    // et on remet à jour l'IHM pour refléter les modifs
     maj_etat(ctx);
 }
 
+static GtkActionEntry menu_entries[] = {
+    /* name, stock id, label, accelerator, tooltip, callback */
+    {"GameMenuAction", NULL, "_Game"},
+    {"NewAction", GTK_STOCK_NEW, "_New", "<control>N", "Start new game", G_CALLBACK(nouvelle_partie)},
+    {"SaveAction", GTK_STOCK_SAVE_AS, "_Save", "<control>S", "Save game", G_CALLBACK(sauver_partie)},
+    {"LoadAction", GTK_STOCK_OPEN, "L_oad", "<control>O", "Load saved game", G_CALLBACK(charger_partie)},
+    {"QuitAction", GTK_STOCK_QUIT, "_Quit", "<control>Q", "Quit game", G_CALLBACK(quitter_partie)},
+
+    {"EditMenuAction", NULL, "E_dit"},
+    {"UndoAction", GTK_STOCK_UNDO, "Undo", "<control>Z", "Undo last action", NULL},
+    {"SettingsAction", GTK_STOCK_PREFERENCES, "Settings", NULL, "Save game", NULL},
+
+    {"HelpMenuAction", NULL, "_Help"},
+    {"ContentsAction", GTK_STOCK_DIALOG_QUESTION, "Contents", "F1", "Manual of the game", NULL},
+    {"AboutAction", GTK_STOCK_ABOUT, "About", NULL, "About the game", NULL},
+};
+
 int main(int argc,char *argv[]) {
+    GError *error;
     JE_contexte ev;
     GdkGeometry hints;
     int i;
@@ -149,37 +179,37 @@ int main(int argc,char *argv[]) {
     gtk_init(&argc,&argv);
 
     // création fenêtre
-    ev.fenetre=gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(ev.fenetre),"jeu d'évasion");
+    ev.fenetre = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(ev.fenetre), "Jeu d'évasion");
     g_signal_connect(G_OBJECT(ev.fenetre), "delete-event", G_CALLBACK(gtk_main_quit), NULL);
 
         // vbox principale
         ev.vbox = gtk_vbox_new(FALSE, 0);
         gtk_container_add(GTK_CONTAINER(ev.fenetre), ev.vbox);
 
-            // hbox du menu
-            ev.hbox_menu = gtk_hbox_new(FALSE, 0);
-            gtk_box_pack_start(GTK_BOX(ev.vbox), ev.hbox_menu, FALSE, FALSE, 0);
+            // menu
+            GtkActionGroup *action_group;
+            GtkUIManager *menu_manager;
+            action_group = gtk_action_group_new("MenuActions");
+            gtk_action_group_add_actions(action_group, menu_entries, G_N_ELEMENTS(menu_entries), &ev);
+            menu_manager = gtk_ui_manager_new();
+            gtk_ui_manager_insert_action_group(menu_manager, action_group, 0);
+            error = NULL;
+            gtk_ui_manager_add_ui_from_file(menu_manager, "menu.ui", &error);
+            if(error) {
+                g_message("building menus failed: %s", error->message);
+                g_error_free(error);
+            }
+            gtk_window_add_accel_group(GTK_WINDOW(ev.fenetre), gtk_ui_manager_get_accel_group(menu_manager));
+            ev.menu = gtk_ui_manager_get_widget(menu_manager, "/MainMenu");
+            gtk_box_pack_start(GTK_BOX(ev.vbox), ev.menu, FALSE, FALSE, 0);
+            gtk_widget_show(ev.menu);
 
-                // bouton nouvelle partie
-                ev.bt_nouvelle_partie = gtk_button_new_with_label("nouvelle partie");
-                g_signal_connect(G_OBJECT(ev.bt_nouvelle_partie), "clicked", G_CALLBACK(nouvelle_partie), &ev);
-                gtk_box_pack_start(GTK_BOX(ev.hbox_menu), ev.bt_nouvelle_partie, FALSE, FALSE, 0);
-                gtk_widget_show(ev.bt_nouvelle_partie);
-
-                // bouton quitter
-                ev.bt_quitter = gtk_button_new_with_label("Quitter");
-                g_signal_connect(G_OBJECT(ev.bt_quitter), "clicked", G_CALLBACK(gtk_main_quit), NULL);
-                gtk_box_pack_end(GTK_BOX(ev.hbox_menu), ev.bt_quitter, FALSE, FALSE, 0);
-                gtk_widget_show(ev.bt_quitter);
-
-            gtk_widget_show(ev.hbox_menu);
-
-            // aspect frame
+            // aspect frame (frame de ratio constant pour être sûr que les cases restent carrées)
             ev.aframe = gtk_aspect_frame_new(NULL, 0.5, 0.5, 9.0/13.0, 0);
             gtk_box_pack_start(GTK_BOX(ev.vbox), ev.aframe, TRUE, TRUE, 0);
 
-                // frame
+                // la table qui contient le plateau de jeu
                 ev.table = gtk_table_new(13, 9, TRUE);
                 gtk_container_add(GTK_CONTAINER(ev.aframe), ev.table);
 
